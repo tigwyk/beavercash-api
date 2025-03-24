bb63ce49cb6122fe96800a5e6c303c90bde4497e
tigwyk <lee@soylentcola.com>
2025-03-23 22:40:08 -0700

    Add API routes

--- index.ts
+++ index.ts
@@ -1,93 +1,24 @@
 import { serve } from 'bun';
+import db from './db';
 
 const PORT = 3000;
 
-interface Post {
-    id: string;
-    title: string;
-    content: string;
-}
-
-interface User {
-    id: string;
-    name: string;
-    email: string;
-}
-
-let blogPosts : Post[] = [];
-let users : User[] = [];
-
-// Post handlers
-function handleGetAllPosts() {
-    return new Response(
-        JSON.stringify(blogPosts), {
-            headers: { 'Content-Type': 'application/json' }
-        });
-}
-
-function handleGetPostById(id: string) {
-    const post = blogPosts.find(post => post.id === id);
-    if (!post) {
-        return new Response('Not Found', { status: 404 });
-    }
-    return new Response(
-        JSON.stringify(post), {
-            headers: { 'Content-Type': 'application/json' }
-        });
-}
-
-function handleCreatePost(title: string, content: string) {
-    const newPost : Post = {
-        id: `${blogPosts.length}`,
-        title,
-        content
-    };
-
-    blogPosts.push(newPost);
-    console.log('New post created:', newPost);
-    return new Response(
-        JSON.stringify(newPost), {
-            headers: { 'Content-Type': 'application/json' },
-            status: 201
-        });
-}
-
-function handleUpdatePost(id: string, title: string, content: string) {
-    const postIndex = blogPosts.findIndex((post) => post.id === id);
-    if (postIndex === -1) {
-        return new Response('Not Found', { status: 404 });
-    }
-
-    blogPosts[postIndex] = {
-        ... blogPosts[postIndex],
-        title,
-        content
-    } as Post
-
-    return new Response('Updated', { status: 200 });
-}
-
-function handleDeletePost(id: string) {
-    const postIndex = blogPosts.findIndex((post) => post.id === id);
-    if (postIndex === -1) {
-        return new Response('Not Found', { status: 404 });
-    }
-
-    blogPosts.splice(postIndex, 1);
-
-    return new Response('Deleted', { status: 200 });
-}
-
 // User handlers
-function handleGetAllUsers() {
+async function handleGetAllUsers() {
     return new Response(
-        JSON.stringify(users), {
+        JSON.stringify(await db.user.findMany({orderBy:{
+            createdAt: 'asc'
+        }})), {
             headers: { 'Content-Type': 'application/json' }
         });
 }
 
-function handleGetUserById(id: string) {
-    const user = users.find(user => user.id === id);
+async function handleGetUserById(id: number) {
+    const user = await db.user.findUnique({
+        where: {
+            id
+        }
+    });
     if (!user) {
         return new Response('Not Found', { status: 404 });
     }
@@ -97,14 +28,14 @@ function handleGetUserById(id: string) {
         });
 }
 
-function handleCreateUser(name: string, email: string) {
-    const newUser : User = {
-        id: `${users.length}`,
-        name,
-        email
-    };
+async function handleCreateUser(name: string, email: string) {
+    const newUser = await db.user.create({
+        data: {
+            name,
+            email
+        }
+    });
 
-    users.push(newUser);
     console.log('New user created:', newUser);
     return new Response(
         JSON.stringify(newUser), {
@@ -113,105 +44,67 @@ function handleCreateUser(name: string, email: string) {
         });
 }
 
-function handleUpdateUser(id: string, name: string, email: string) {
-    const userIndex = users.findIndex((user) => user.id === id);
-    if (userIndex === -1) {
-        return new Response('Not Found', { status: 404 });
-    }
-
-    users[userIndex] = {
-        ...users[userIndex],
-        name,
-        email
-    } as User
+async function handleUpdateUser(id: number, options: { name?: string, email?: string}) {
+    const { name, email } = options;
+    await db.user.update({
+        where: {
+            id
+        },
+        data: {
+            ... (name ? { name } : {}),
+            ... (email ? { email } : {})
+        }
+    });
 
     return new Response('Updated', { status: 200 });
 }
 
-function handleDeleteUser(id: string) {
-    const userIndex = users.findIndex((user) => user.id === id);
-    if (userIndex === -1) {
-        return new Response('Not Found', { status: 404 });
-    }
-
-    users.splice(userIndex, 1);
+async function handleDeleteUser(id: number) {
+    await db.user.delete({
+        where: {
+            id
+        }
+    });
 
     return new Response('Deleted', { status: 200 });
 }
 
 serve({
     port: PORT,
-    async fetch(request) {
-        const { method } = request;
-        const { pathname } = new URL(request.url);
-        const postIDRegex = /^\/api\/posts\/(\d+)$/;
-        const userIDRegex = /^\/api\/users\/(\d+)$/;
-
-        if (method === 'GET' && pathname === '/api/hello') {
-            return new Response('Hello, World!');
-        }
-        if (method === 'GET' && pathname === '/api/goodbye') {
-            return new Response('Goodbye, World!');
-        }
-        
-        // User routes
-        if (method === 'GET' && pathname === '/api/users') {
-            return handleGetAllUsers();
-        }
-        if (method === 'GET' && pathname.match(userIDRegex)) {
-            const userID = pathname.match(userIDRegex)?.[1];
-            if (userID) {
-                return handleGetUserById(userID);
-            }
-        }
-        if (method === 'POST' && pathname === '/api/users') {
-            const body = await request.json();
+    routes: {
+        // Static routes
+        "/api/status": new Response("OK"),
+    
+        // Dynamic routes
+        "/api/users/:id": async req => {
+            const id = req.params.id;
+            return handleGetUserById(parseInt(id));
+                },
+        "/api/users" : {
+        GET: () => handleGetAllUsers(),
+        POST: async req => {
+            const body = await req.json();
             return handleCreateUser(body.name, body.email);
+        },
+        PUT: async req => {
+            const body = await req.json();
+            return handleUpdateUser(parseInt(body.id), { name: body.name, email: body.email });
+        },
+        DELETE: async req => {
+            const body = await req.json();
+            return handleDeleteUser(parseInt(body.id));
         }
-        if (method === 'UPDATE' && pathname.match(userIDRegex)) {
-            const userID = pathname.match(userIDRegex)?.[1];
-            if (userID) {
-                const body = await request.json();
-                return handleUpdateUser(userID, body.name, body.email);
-            }
-        }
-        if (method === 'DELETE' && pathname.match(userIDRegex)) {
-            const userID = pathname.match(userIDRegex)?.[1];
-            if (userID) {
-                return handleDeleteUser(userID);
-            }
-        }
-        
-        // Post routes
-        if (method === 'GET' && pathname === '/api/posts') {
-            return handleGetAllPosts();
-        }
-        if (method === 'GET' && pathname.match(postIDRegex)) {
-            const postID = pathname.match(postIDRegex)?.[1];
-            if (postID) {
-                return handleGetPostById(postID);
-            }    
-        }
-        if (method === 'POST' && pathname === '/api/posts') {
-            const body = await request.json();
-            return handleCreatePost(body.title, body.content);
-        }
-        if (method === 'UPDATE' && pathname.match(postIDRegex)) {
-            const postID = pathname.match(postIDRegex)?.[1];
-            if (postID) {
-                const body = await request.json();
-                return handleUpdatePost(postID, body.title, body.content);
-            }
-        }
-        if (method === 'DELETE' && pathname.match(postIDRegex)) {
-            const postID = pathname.match(postIDRegex)?.[1];
-            if (postID) {
-                return handleDeletePost(postID);
-            }
-        }
-        
-        return new Response('Not Found', { status: 404 });
-    },
+        },
+    
+        // Wildcard route for all routes that start with "/api/" and aren't otherwise matched
+        "/api/*": Response.json({ message: "Not found" }, { status: 404 }),
+      },
+    
+      // (optional) fallback for unmatched routes:
+      // Required if Bun's version < 1.2.3
+      fetch(req) {
+        return new Response("Not Found", { status: 404 });
+      },
 });
 
-console.log(`Server running at http://localhost:${PORT}`);
+console.log(`BeavercashAPI Server running at http://localhost:${PORT}`);
